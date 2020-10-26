084bb997229a9c47a708f4c29b8890c0f0464f33
MidnightCaffeine <abhishek.akc7@gmail.com>
2020-10-27 04:08:24 +0530

    Some configuration changes

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
-require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
+// CORS Middleware
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+
+// ENV data
+require("dotenv").config();
 
 const uri = process.env.MERNAUTH_ATLAS_URI;
 const port = process.env.PORT || 5000;
@@ -20,10 +25,10 @@ connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
-const usersRouter = require('./routes/users.route');
-app.use('/users', usersRouter);
-const todosRouter = require('./routes/todos.route');
-app.use('/todos', todosRouter);
+const usersRouter = require("./routes/users.route");
+app.use("/users", usersRouter);
+const todosRouter = require("./routes/todos.route");
+app.use("/todos", todosRouter);
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
